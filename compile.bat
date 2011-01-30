655921f8480befb7834581b8f65bb42c34af9ad7
ssanner <ssanner@563501be-96f2-e153-6a30-f423d0ec6236>
2011-01-30 13:14:53 +0000

    Minor updates

--- compile.bat
+++ compile.bat
@@ -5,4 +5,5 @@ set LIB_HOME=%LOCAL_HOME%\lib
 set LIBJARS=%LOCAL_HOME%\src
 set BINDIR=%LOCAL_HOME%\bin
 for %%i in (%LIB_HOME%\*.jar) do call %LOCAL_HOME%\cpappend.bat %%i
+mkdir %BINDIR%
 javac -classpath %LIBJARS% -d %BINDIR% %LOCAL_HOME%\src\rddl\*.java %LOCAL_HOME%\src\rddl\competition\*.java %LOCAL_HOME%\src\rddl\parser\*.java %LOCAL_HOME%\src\rddl\policy\*.java %LOCAL_HOME%\src\rddl\translate\*.java %LOCAL_HOME%\src\rddl\validate\*.java %LOCAL_HOME%\src\rddl\viz\*.java
