673fcf12e6e738742db6d1c54fecf906f55e997e
ssanner <ssanner@563501be-96f2-e153-6a30-f423d0ec6236>
2011-04-20 18:57:57 +0000

    Added sample final competition domains / instances

---
+++ trunk/files/final_comp/test_domains.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy elevators_inst_mdp__9 rddl.viz.ElevatorDisplay
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy elevators_inst_pomdp__9 rddl.viz.ElevatorDisplay
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy game_of_life_inst_mdp__10 rddl.viz.GameOfLifeScreenDisplay
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy game_of_life_inst_pomdp__10 rddl.viz.GameOfLifeScreenDisplay
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy sysadmin_inst_mdp__10 rddl.viz.SysAdminScreenDisplay
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy sysadmin_inst_pomdp__10 rddl.viz.SysAdminScreenDisplay
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy traffic_inst_mdp__10 rddl.viz.TrafficDisplay
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy traffic_inst_pomdp__10 rddl.viz.TrafficDisplay
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy crossing_traffic_inst_mdp__10 rddl.viz.CrossingTrafficDisplay
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy crossing_traffic_inst_pomdp__10 rddl.viz.CrossingTrafficDisplay
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy recon_inst_mdp__10
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy recon_inst_pomdp__10
+
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy skill_teaching_inst_mdp__10
+./run rddl.sim.Simulator files/final_comp/rddl rddl.policy.RandomBoolPolicy skill_teaching_inst_pomdp__10
