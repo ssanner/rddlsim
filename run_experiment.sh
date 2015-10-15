52e415a2a28543945f672fc50ce473ba12ae3636
Aswin NadamuniRaghavan <nadamuna@submit-em64t-01.hpc.engr.oregonstate.edu>
2015-10-14 17:45:38 -0700

    cluster license file loc

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,9 +27,12 @@ set futureStrat = FUTURE_STRATEGY
 set instanceName = `cat $inst | grep "instance " | cut -f 2 -d ' ' | cut -f 1 -d ' '`
 set viz = VISUALIZER 
 
-setenv GRB_LICENSE_FILE /scratch/rddlsim/lib/gurobi605/linux64/gurobi.lic
-setenv LD_LIBRARY_PATH /scratch/rddlsim/lib/gurobi605/linux64/lib
-set path = ($path /scratch/rddlsim/lib/gurobi605/linux64/bin)
+setenv LD_LIBRARY_PATH /scratch/cluster-share/gurobi604/linux64/lib
+set path = ( $path /scratch/cluster-share/gurobi604/linux64/bin )
+setenv GRB_LICENSE_FILE /nfs/cluster-fserv/cluster-share/gurobi604/linux64/license/gurobi.lic
+#setenv GRB_LICENSE_FILE /sciratch/rddlsim/lib/gurobi66/linux64/gurobi.lic
+#setenv LD_LIBRARY_PATH /scratch/rddlsim/lib/gurobi605/linux64/lib
+#set path = ($path /scratch/rddlsim/lib/gurobi605/linux64/bin)
 
 set files_dir = ./files/mdp_hybrid/
 set portno = SERVER_PORT_NUMBER
@@ -52,12 +55,16 @@ limit
 
 echo " solving $dom $inst"
 
-time /usr/local/common64/jdk1.8/bin/java -jar -Xmx4g -Xms1g -ea /tmp/$job_title\_tmp/RDDLServer.jar $files_dir $portno $nrounds $seed >! /tmp/$job_title\_tmp/$job_title\_server.log & 
+time /usr/local/common64/jdk1.8/bin/java -jar -Xmx1g -Xms1g -ea /tmp/$job_title\_tmp/RDDLServer.jar $files_dir $portno $nrounds $seed >! /tmp/$job_title\_tmp/$job_title\_server.log & 
 set server_pid = `echo $!`  
+echo "server pid $server_pid"
  
 sleep 10
 
 time /usr/local/common64/jdk1.8/bin/java -jar -Xmx4g -Xms1g -ea /tmp/$job_title\_tmp/HOPTranslateRDDLClient.jar $files_dir localhost grb_$job_title rddl.det.mip.HOPTranslate $portno $seed $instanceName $dom $inst $lookahead $timeOutMins $viz $numFutures $futureStrat $rootPolicy >! /tmp/$job_title\_tmp/$job_title\_client.log  
+
+echo "killing pid $server_pid"
+kill -9 $server_pid 
 
 cp -f /tmp/$job_title\_tmp/$job_title*.log ./log/
 cp -f JOB_NAME.out ./log/
@@ -68,7 +75,7 @@ cp -f reservoir_rain_last.viz log/$job_title\_rain.viz
 rm -f /tmp/$job_title\_tmp/*
 rmdir -f /tmp/$job_title\_tmp
 
-ps aux | grep -i RDDLServer | cut -f 2 -d ' ' | head -n 1 | xargs kill -9
+#ps aux | grep -i RDDLServer | cut -f 2 -d ' ' | head -n 1 | xargs kill -9
 
 echo "HOP PLanning done $dom $inst"
 
