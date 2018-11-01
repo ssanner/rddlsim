bfca7d12518620cc06ac13ad629c9ea61cde741e
parth <parthjaggi28@gmail.com>
2018-11-01 15:42:13 +0530

    visualization edits;

--- src/rddl/viz/AmbulanceDisplay.java
+++ src/rddl/viz/AmbulanceDisplay.java
@@ -47,16 +47,20 @@ public class AmbulanceDisplay extends StateViz {
 
 	public String getStateDescription(State s) throws EvalException {
 		StringBuilder sb = new StringBuilder();
+		
+		TYPE_NAME ambl_type = new TYPE_NAME("ambulance");
+		ArrayList<LCONST> ambulances = s._hmObject2Consts.get(ambl_type);
 
 		TYPE_NAME xpos_type = new TYPE_NAME("x_pos");
 		ArrayList<LCONST> list_xpos = s._hmObject2Consts.get(xpos_type);
 
 		TYPE_NAME ypos_type = new TYPE_NAME("y_pos");
 		ArrayList<LCONST> list_ypos = s._hmObject2Consts.get(ypos_type);
-		
-		PVAR_NAME TARGET = new PVAR_NAME("TARGET");
-		PVAR_NAME burning = new PVAR_NAME("burning");
-		PVAR_NAME out_of_fuel = new PVAR_NAME("out-of-fuel");
+
+		PVAR_NAME HOSPITAL = new PVAR_NAME("HOSPITAL");
+		PVAR_NAME emergency_at = new PVAR_NAME("emergency-at");
+		PVAR_NAME ambulance_at = new PVAR_NAME("ambulance-at");
+		PVAR_NAME with_patient = new PVAR_NAME("with-patient");
 
 		if (_bd == null) {
 			int max_row = list_ypos.size() - 1;
@@ -66,36 +70,55 @@ public class AmbulanceDisplay extends StateViz {
 		}
 		
 		// Set up an arity-1 parameter list
-		ArrayList<LCONST> params = new ArrayList<LCONST>(2);
-		params.add(null);
-		params.add(null);
+		ArrayList<LCONST> params1 = new ArrayList<LCONST>(2);
+		params1.add(null);
+		params1.add(null);
+
+		ArrayList<LCONST> params2 = new ArrayList<LCONST>(3);
+		params2.add(null);
+		params2.add(null);
+		params2.add(null);
+
+		ArrayList<LCONST> params3 = new ArrayList<LCONST>(1);
+		params3.add(null);
 
 		_bd.clearAllCells();
 		_bd.clearAllLines();
-		for (LCONST xpos : list_xpos) {
-			for (LCONST ypos : list_ypos) {
-				int col = new Integer(xpos.toString().substring(2, xpos.toString().length()));
-				int row = new Integer(ypos.toString().substring(2, ypos.toString().length())) - 1;
-				params.set(0, xpos);
-				params.set(1, ypos);
-				boolean b_is_target   = (Boolean)s.getPVariableAssign(TARGET, params);
-				boolean b_burning     = (Boolean)s.getPVariableAssign(burning, params);
-				boolean b_out_of_fuel = (Boolean)s.getPVariableAssign(out_of_fuel, params);
-				
-				String letter = null;
-				if (b_is_target)
-					letter = "T";
-				
-				Color color = Color.green;
-				
-				if (b_burning && b_out_of_fuel)
-					color = new Color(139, 69, 19); // brown
-				else if (b_burning) 
-					color = Color.red;
-				else if (b_out_of_fuel)
-					color = Color.black;
-				
-				_bd.setCell(row, col, color, letter);
+		for (LCONST ambl : ambulances) {
+			for (LCONST xpos : list_xpos) {
+				for (LCONST ypos : list_ypos) {
+					int col = new Integer(xpos.toString().substring(2, xpos.toString().length()));
+					int row = new Integer(ypos.toString().substring(2, ypos.toString().length())) - 1;
+
+					params1.set(0, xpos);
+					params1.set(1, ypos);
+					params2.set(0, ambl);
+					params2.set(1, xpos);
+					params2.set(2, ypos);
+					params3.set(0, ambl);
+
+					boolean b_is_hospital  = (Boolean)s.getPVariableAssign(HOSPITAL, params1);
+					boolean b_emergency_at = (Boolean)s.getPVariableAssign(emergency_at, params1);
+					boolean b_ambulance_at = (Boolean)s.getPVariableAssign(ambulance_at, params2);
+					boolean b_with_patient = (Boolean)s.getPVariableAssign(with_patient, params3);
+					
+					String letter = null;
+					if (b_is_hospital)
+						letter = "H";
+					
+					Color color = Color.green;
+					
+					if (b_emergency_at && b_ambulance_at)
+						color = Color.pink;
+					else if (b_ambulance_at && b_with_patient) 
+						color = Color.yellow;
+					else if (b_emergency_at) 
+						color = Color.red;
+					else if (b_ambulance_at)
+						color = Color.white;
+					
+					_bd.setCell(row, col, color, letter);
+				}
 			}
 		}
 			
